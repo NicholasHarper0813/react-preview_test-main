0009c732ed7bce6efdbf9c0ba702d86360811953
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-11 23:06:23 -0800

    Update App.js

--- test-react-app/src/App.js
+++ test-react-app/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
-class App extends Component {
+class App extends Component 
+{
   render() {
     return (
       <div className="App">
-	      This is my app
+	      Welcome my app
       </div>
     );
   }
